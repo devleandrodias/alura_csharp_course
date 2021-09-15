95a10595a39f8b313bec68fc60ac55a72913ce6d
devleandrodias <leandrodbdias@gmail.com>
2021-09-15 08:35:54 -0700

    Create new solution and create first class

---
+++ ByteBank/ByteBank_1/CheckingAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace ByteBank_1
+{
+    public class CheckingAccount
+    {
+        public string Owner { get; set; }
+        public int Agency { get; set; }
+        public int Number { get; set; }
+        public double Balance { get; set; }
+    }
+}
---
+++ ByteBank/ByteBank_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ByteBank_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckingAccount checkingAccountLeandro = new();
+
+            checkingAccountLeandro.Owner = "Leandro";
+            checkingAccountLeandro.Agency = 863;
+            checkingAccountLeandro.Number = 863146;
+            checkingAccountLeandro.Balance = 100;
+        }
+    }
+}
