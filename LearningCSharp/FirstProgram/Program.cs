3f7f1215b31f549ee4c5adb0ea3a1a703cc62b29
devleandrodias <leandrodbdias@gmail.com>
2021-09-14 17:42:49 -0700

    create solution and first program using visual studio

---
+++ LearningCSharp/FirstProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Project in Visual Studio 2022!");
+
+            Console.ReadLine();
+        }
+    }
+}
