b8e065a9e00cfe6a9987765dfb63163d59ea4f0e
devleandrodias <leandrodbdias@gmail.com>
2021-09-14 21:08:26 -0700

    learning about chars and strings

---
+++ LearningCSharp/Assignments/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execution 6 program - Assignments");
+
+            int age = 32;
+
+            int ageLeandro = age;
+
+            age = 20;
+
+            Console.WriteLine(age);
+
+            Console.WriteLine(ageLeandro);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LearningCSharp/Texts/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Texts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execution 5 program - Character and strings");
+
+            char firstLetter = (char)(75 + 8);
+
+            Console.WriteLine(firstLetter);
+
+            string company = "Alura school";
+
+            Console.WriteLine(company);
+
+            Console.ReadLine();
+        }
+    }
+}
