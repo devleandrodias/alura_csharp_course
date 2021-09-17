a0cb24791fbc1d2271e54a59bc0848d95be46966
devleandrodias <leandrodbdias@gmail.com>
2021-09-16 17:18:43 -0700

    learning about functions and methods

---
+++ ByteBank/ByteBank_04/CheckingAccount.cs
@@ -0,0 +1,46 @@
+﻿namespace ByteBank_04
+{
+    public class CheckingAccount
+    {
+        public CheckingAccount()
+        {
+            Balance = 100;
+        }
+
+        public string Owner { get; set; }
+        public int Agency { get; set; }
+        public int Number { get; set; }
+        public double Balance { get; set; }
+
+        public bool Withdraw(double value)
+        {
+            if (Balance < value)
+            {
+                return false;
+            }
+
+            Balance -= value;
+
+            return true;
+        }
+
+        public void Deposit(double value)
+        {
+            Balance += value;
+        }
+
+        public bool Transfer(double value, CheckingAccount accountDestiny)
+        {
+            if (Balance < value)
+            {
+                return false;
+            }
+
+            Balance -= value;
+
+            accountDestiny.Deposit(value);
+
+            return true;
+        }
+    }
+}
---
+++ ByteBank/ByteBank_04/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ByteBank_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckingAccount checkingAccountLeandro = new() {
+                Owner = "Leandro",
+                Balance = 254.23,
+                Agency = 234,
+                Number = 234534
+            };
+
+            CheckingAccount checkingAccountThaisa = new()
+            {
+                Owner = "Thaísa",
+                Balance = 345.45,
+                Agency = 234,
+                Number = 234864
+            };
+
+            Console.WriteLine(checkingAccountLeandro.Balance);
+
+            bool canWithdraw = checkingAccountLeandro.Withdraw(60);
+
+            Console.WriteLine($"Can withdraw? {canWithdraw}");
+
+            Console.WriteLine(checkingAccountLeandro.Balance);
+
+            checkingAccountLeandro.Deposit(45.23);
+
+            checkingAccountLeandro.Deposit(783.34);
+
+            Console.WriteLine(checkingAccountLeandro.Balance);
+
+            checkingAccountThaisa.Transfer(231.34, checkingAccountLeandro);
+
+            Console.WriteLine(checkingAccountLeandro.Balance.ToString("C2"));
+
+            Console.ReadLine();
+        }
+    }
+}
