f3a9a2620af1c7354e8bf1406027d3d868f2311e
devleandrodias <leandrodbdias@gmail.com>
2021-09-16 18:01:07 -0700

    learning about namespaces, usings and null references

---
+++ ByteBank/ByteBank_05/CheckingAccount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ByteBank_05
+{
+    public class CheckingAccount
+    {
+        public CheckingAccount()
+        {
+            Balance = 100;
+        }
+
+        public Client Client { get; set; }
+
+        public int Agency { get; set; }
+
+        public int Number { get; set; }
+
+        public double Balance { get; set; }
+
+        public bool Withdraw(double value)
+        {
+            if (Balance < value)
+            {
+                return false;
+            }
+
+            Balance -= value;
+
+            return true;
+        }
+
+        public void Deposit(double value)
+        {
+            Balance += value;
+        }
+
+        public bool Transfer(double value, CheckingAccount accountDestiny)
+        {
+            if (Balance < value)
+            {
+                return false;
+            }
+
+            Balance -= value;
+
+            accountDestiny.Deposit(value);
+
+            return true;
+        }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine($"{Client.Name} - {Balance:C2}");
+        }
+    }
+}
---
+++ ByteBank/ByteBank_05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ByteBank_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client leandro = new()
+            {
+                Name = "Leandro",
+                Job = "Sr. Backend Software Engineer",
+                Cpf = "111.111.111-11"
+            };
+
+            CheckingAccount checkingAccountLeandro = new()
+            {
+                Client = leandro,
+                Agency = 145,
+                Number = 145673,
+                Balance = 5234.34,
+            };
+
+            checkingAccountLeandro.Deposit(200);
+
+            checkingAccountLeandro.Withdraw(50);
+
+            checkingAccountLeandro.ShowBalance();
+
+            Console.ReadLine();
+        }
+    }
+}
