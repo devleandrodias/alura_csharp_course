eadaad1a283125a0fb4db6daa1a4ba0fe5bbb6de
devleandrodias <leandrodbdias@gmail.com>
2021-09-17 10:03:11 -0700

    learning about getters and setters

---
+++ ByteBank/ByteBank_06/CheckingAccount.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ByteBank_06
+{
+    public class CheckingAccount
+    {
+        private Client _client;
+
+        public Client Client
+        {
+            get
+            {
+                return _client;
+            }
+
+            set
+            {
+                _client = value;
+            }
+        }
+
+        public int Agency { get; set; }
+
+        public int Number { get; set; }
+
+        private double _balance = 100;
+
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _balance = value;
+            }
+        }
+
+        public bool Withdraw(double value)
+        {
+            if (_balance < value)
+            {
+                return false;
+            }
+
+            _balance -= value;
+
+            return true;
+        }
+
+        public void Deposit(double value)
+        {
+            _balance += value;
+        }
+
+        public bool Transfer(double value, CheckingAccount accountDestiny)
+        {
+            if (_balance < value)
+            {
+                return false;
+            }
+
+            _balance -= value;
+
+            accountDestiny.Deposit(value);
+
+            return true;
+        }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine($"{_client.Name} - {_balance:C2}");
+        }
+    }
+}
---
+++ ByteBank/ByteBank_06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ByteBank_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client= new ()
+            {
+                Name = "Leandro",
+                Cpf = "111.111.111-11",
+                Job = "Dev"
+            };
+
+            CheckingAccount account = new();
+
+            account.Client = client;
+
+            account.Balance = 10;
+
+            Console.WriteLine(account.Balance);
+
+            account.Deposit(20);
+
+            account.Deposit(15.23);
+
+            account.Withdraw(53.21);
+
+            account.ShowBalance();
+
+            Console.ReadLine();
+        }
+    }
+}
