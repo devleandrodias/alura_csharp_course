28771cff485709cc3c55e03de31863e1433c02aa
devleandrodias <leandrodbdias@gmail.com>
2021-10-03 11:12:01 -0300

    learning about referencing DLL

---
+++ ByteBank.InternalSystem/ByteBank.InternalSystem/Program.cs
@@ -0,0 +1,26 @@
+﻿using ByteBank.Models;
+using System;
+
+namespace ByteBank.InternalSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckingAccount account = new(0001, 123567)
+            {
+                Balance = 6233,
+                Client = new Client
+                {
+                    Cpf = "111.111.111-11",
+                    Job = "Software engineer",
+                    Name = "Leandro Dias"
+                }
+            };
+
+            account.ShowBalance();
+
+            Console.ReadLine();
+        }
+    }
+}
