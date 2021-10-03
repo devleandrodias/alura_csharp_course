f4eb3330db1ffe7a2e3401410ab63293306bb30f
devleandrodias <leandrodbdias@gmail.com>
2021-10-03 13:45:11 -0300

    learning about how to use external libries

--- ByteBank/ByteBank.AgencySystem/Program.cs
+++ ByteBank/ByteBank.AgencySystem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ByteBank.Models;
 using ByteBank.Models.Employees;
+using Humanizer;
 using System;
 
 namespace ByteBank.AgencySystem
@@ -7,6 +8,17 @@ namespace ByteBank.AgencySystem
     internal class Program
     {
         static void Main()
+        {
+            DateTime paymentEndDate = new(2021, 11, 14);
+
+            TimeSpan difference = paymentEndDate - DateTime.Now;
+
+            Console.WriteLine(TimeSpanHumanizeExtensions.Humanize(difference));
+
+            Console.ReadLine();
+        }
+
+        private static void Documentation()
         {
             CheckingAccount checkingAccount = new(0001, 12893);
 
@@ -17,8 +29,6 @@ namespace ByteBank.AgencySystem
             Console.WriteLine(employee.Name);
 
             Console.WriteLine(checkingAccount.Number);
-
-            Console.ReadLine();
         }
     }
 }
