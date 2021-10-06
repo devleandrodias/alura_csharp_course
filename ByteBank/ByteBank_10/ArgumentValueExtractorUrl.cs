f61b5cc4a5feeb47aa831239d3354c5ee5ceda8d
devleandrodias <leandrodbdias@gmail.com>
2021-10-06 15:54:08 -0300

    learning about string methods, indexOf, contains, startWith, endEith, toUpper, toLower, remve, etc..

---
+++ ByteBank/ByteBank_10/ArgumentValueExtractorUrl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ByteBank_10
+{
+    public class ArgumentValueExtractorUrl
+    {
+        private readonly string _arguments;
+
+        public string Url { get; }
+
+        public ArgumentValueExtractorUrl(string url)
+        {
+            url = url.ToLower();
+
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("URL argument cannot be null or empty.", nameof(url));
+            }
+
+            if (!url.StartsWith("https"))
+            {
+                throw new ArgumentException("HTTP not supported only HTTPS", nameof(url));
+            }
+
+            if (!url.Contains("https://bytebank.com"))
+            {
+                throw new ArgumentException("Unknown origin url", nameof(url));
+            }
+
+            int index = url.IndexOf('?');
+
+            _arguments = url[(index + 1)..];
+
+            Url = url;
+        }
+
+        public string GetCurrency(string currency)
+        {
+            string search = currency.ToUpper() + "=";
+
+            string arugmentUpper = _arguments.ToUpper();
+
+            int searchIndex = arugmentUpper.IndexOf(search);
+
+            string result = arugmentUpper.Substring(searchIndex + search.Length);
+
+            int ampersandIndex = result.IndexOf('&');
+
+            if (ampersandIndex == -1) return result;
+
+            return result.Remove(ampersandIndex);
+        }
+    }
+}
---
+++ ByteBank/ByteBank_10/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ByteBank_10
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string url = "https://bytebank.com/currencyOrigin=BRL&CURrencyDestiny=CAD&vaLue=2000";
+
+            string urlNubank = "https://nubank.com/currencyOrigin=BRL&CURrencyDestiny=CAD&vaLue=2000";
+
+            ArgumentValueExtractorUrl extractor = new(urlNubank);
+
+            Console.WriteLine("Value - " + extractor.GetCurrency("Value"));
+            Console.WriteLine("Origin - " + extractor.GetCurrency("CUrreNCYORigiN"));
+            Console.WriteLine("Destiny - " + extractor.GetCurrency("currencyDestiny"));
+
+            Console.ReadLine();
+        }
+
+        private static void Introduction()
+        {
+            string url = "page?args";
+
+            int index = url.IndexOf('?');
+
+            try
+            {
+                ArgumentValueExtractorUrl extractorUrl = new("");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            string arguments = url.Substring(index + 1);
+
+            Console.WriteLine(url);
+
+            Console.WriteLine(arguments); // string is immutable
+
+            Console.WriteLine(url[(1 + index)..]);
+        }
+    }
+}
