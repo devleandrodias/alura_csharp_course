3ce5ea0ac9baf83b1858854daa4c14c7aa276aef
devleandrodias <leandrodbdias@gmail.com>
2021-10-08 13:50:26 -0300

    learning about regex methods

--- ByteBank/ByteBank_10/Program.cs
+++ ByteBank/ByteBank_10/Program.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace ByteBank_10
 {
     internal class Program
     {
         static void Main()
+        {
+            Console.ReadLine();
+        }
+
+        private static void RegexMethods()
+        {
+            string input = "Hello my name is Leandro, call me in 92688-2345";
+
+            string pattern0 = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+
+            string pattern1 = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+
+            string pattern2 = "[0-9]{4}[-][0-9]{4}";
+
+            string pattern3 = "[0-9]{4,5}[-][0-9]{4}";
+
+            string pattern4 = "[0-9]{4,5}[-]{0,1}[0-9]{4}";
+
+            string pattern5 = "[0-9]{4,5}[-]?[0-9]{4}";
+
+            Match match = Regex.Match(input, pattern5);
+
+            Console.WriteLine(match.Value);
+        }
+
+        private static void StringMethods()
         {
             string url = "https://bytebank.com/currencyOrigin=BRL&CURrencyDestiny=CAD&vaLue=2000";
 
@@ -15,8 +42,6 @@ namespace ByteBank_10
             Console.WriteLine("Value - " + extractor.GetCurrency("Value"));
             Console.WriteLine("Origin - " + extractor.GetCurrency("CUrreNCYORigiN"));
             Console.WriteLine("Destiny - " + extractor.GetCurrency("currencyDestiny"));
-
-            Console.ReadLine();
         }
 
         private static void Introduction()
