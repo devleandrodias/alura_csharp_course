0d9441e57636cf9a00e83d45b826cd5e2d58db0d
devleandrodias <leandrodbdias@gmail.com>
2021-10-12 11:27:28 -0300

    learning about list, sort, IComparable and IComparer

---
+++ ByteBank/ByteBank_12/CheckingAccount.cs
@@ -0,0 +1,125 @@
+﻿using ByteBank_12.Exceptions;
+using System;
+
+namespace ByteBank_12
+{
+    public class CheckingAccount : IComparable
+    {
+        public static double TransactionFee { get; private set; }
+
+        public CheckingAccount(int agency, int number)
+        {
+            if (agency <= 0)
+            {
+                throw new ArgumentException("Argument agency must be greater than zero.", nameof(agency));
+            }
+
+            if (number <= 0)
+            {
+                throw new ArgumentException("Argument number must be greater than zero.", nameof(number));
+            }
+
+            Agency = agency;
+
+            Number = number;
+
+            TotalAccount++;
+
+            TransactionFee = 30 / TotalAccount;
+        }
+
+        public static int TotalAccount { get; private set; }
+
+        private Client _client;
+
+        public Client Client
+        {
+            get
+            {
+                return _client;
+            }
+
+            set
+            {
+                _client = value;
+            }
+        }
+
+        public int Agency { get; }
+
+        public int Number { get; }
+
+        private double _balance = 100;
+
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _balance = value;
+            }
+        }
+
+        public void Withdraw(double value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Invalid value for withdrawal.", nameof(value));
+            }
+
+            if (_balance < value)
+            {
+                throw new InsufficientFundsException(Balance, value);
+            }
+
+            _balance -= value;
+        }
+
+        public void Deposit(double value)
+        {
+            _balance += value;
+        }
+
+        public void Transfer(double value, CheckingAccount accountDestiny)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Invalid value for transfer.", nameof(value));
+            }
+
+            Withdraw(value);
+
+            accountDestiny.Deposit(value);
+        }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine($"{_client.Name} - {_balance:C2}");
+        }
+
+        public override string ToString()
+        {
+            return $"Number account: {Number} - Agency number: {Agency} - Owner: {Client.Name}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is not CheckingAccount account) { return -1; }
+
+            if (Number < account.Number) { return -1; }
+
+            if (Number == account.Number) { return 0; }
+
+            return 1;
+        }
+    }
+}
--- ByteBank/ByteBank_12/Program.cs
+++ ByteBank/ByteBank_12/Program.cs
@@ -1,4 +1,5 @@
-﻿using ByteBank_12.Extensions;
+﻿using ByteBank_12.Comparators;
+using ByteBank_12.Extensions;
 using System;
 using System.Collections.Generic;
 
@@ -7,6 +8,71 @@ namespace ByteBank_12
     internal class Program
     {
         static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+
+        private static void TestSort()
+        {
+            var accounts = new List<CheckingAccount>()
+            {
+                new (4536, 34576) {Client = new() {Name = "Leandro"}},
+                new (7435, 34074) {Client = new() {Name = "Beatriz"}},
+                new (8534, 97364) {Client = new() {Name = "Thaísa"}},
+                new (2346, 26384) {Client = new() {Name = "Ana"}},
+            };
+
+            accounts.Sort(new CheckingAccountComparatorByAgency());
+
+            foreach (var account in accounts)
+            {
+                Console.WriteLine(account);
+            }
+        }
+
+        private static void TestVarAndSort()
+        {
+            var account1 = new CheckingAccount(0001, 242355);
+            var account2 = new CheckingAccount(0001, 242355);
+            var account3 = new CheckingAccount(0001, 242355);
+
+            var client1 = new Client();
+            var client2 = new Client();
+
+            var list = new List<int>();
+
+            list.Add(1);
+            list.Add(5);
+            list.Add(8);
+            list.Add(3);
+            list.Add(0);
+
+            list.Sort();
+
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            var names = new List<string>()
+            {
+                "Leandro",
+                "Ana",
+                "Beatriz",
+                "Thaísa",
+            };
+
+            names.Sort();
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        private static void TestListMethods()
         {
             List<int> ages1 = new();
             List<int> ages2 = new();
