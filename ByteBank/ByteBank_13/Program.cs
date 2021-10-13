10ad68c128c2d9d50846cae164c98742f2857f9c
devleandrodias <leandrodbdias@gmail.com>
2021-10-13 17:36:39 -0300

    learning about stream reader, partial class, Read, ReadLine, ReadToEnd and EndOfStream

--- ByteBank/ByteBank_13/Program.cs
+++ ByteBank/ByteBank_13/Program.cs
@@ -1,26 +1,24 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 
 namespace ByteBank_13
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
             string pathFile = "C:/Users/MiningRig/Desktop/Study/Alura/alura_csharp_course/files/accounts.txt";
 
-            FileStream fs = new(pathFile, FileMode.Open);
-
-            byte[] buffer = new byte[1024];
-
-            int readBytes = fs.Read(buffer, 0, 1024);
-
-            Encoding utf8 = Encoding.UTF8;
-
-            string text = utf8.GetString(buffer, 0, readBytes);
-
-            Console.Write(text);
+            using (FileStream fs = new(pathFile, FileMode.Open))
+            {
+                using (StreamReader reader = new(fs))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        Console.WriteLine(reader.ReadLine());
+                    }
+                };
+            }
 
             Console.ReadLine();
         }
---
+++ ByteBank/ByteBank_13/WorkWithFileStreamAndBuffers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ByteBank_13
+{
+    internal partial class Program
+    {
+        private static void WorkWithBuffers(string pathFile)
+        {
+            using (FileStream fs = new(pathFile, FileMode.Open))
+            {
+                byte[] buffer = new byte[1024];
+
+                int readBytes = -1;
+
+                while (readBytes != 0)
+                {
+                    readBytes = fs.Read(buffer, 0, 1024);
+
+                    WriteBuffer(buffer, readBytes);
+                }
+            }
+        }
+
+        static void WriteBuffer(byte[] buffer, int readBytes)
+        {
+            Encoding utf8 = Encoding.UTF8;
+
+            string text = utf8.GetString(buffer, 0, readBytes);
+
+            Console.Write(text);
+        }
+    }
+}
