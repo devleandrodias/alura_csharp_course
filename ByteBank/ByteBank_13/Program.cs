48d6a7b20f8fa7c7e95e27520e0ff7bfaaa26f0c
devleandrodias <leandrodbdias@gmail.com>
2021-10-13 17:10:24 -0300

    learning about read file text using Stream

---
+++ ByteBank/ByteBank_13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ByteBank_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathFile = "C:/Users/MiningRig/Desktop/Study/Alura/alura_csharp_course/files/accounts.txt";
+
+            FileStream fs = new(pathFile, FileMode.Open);
+
+            byte[] buffer = new byte[1024];
+
+            int readBytes = fs.Read(buffer, 0, 1024);
+
+            Encoding utf8 = Encoding.UTF8;
+
+            string text = utf8.GetString(buffer, 0, readBytes);
+
+            Console.Write(text);
+
+            Console.ReadLine();
+        }
+    }
+}
