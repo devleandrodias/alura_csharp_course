b0d92ad52d7c1c037356428babef78885e98cf11
devleandrodias <leandrodbdias@gmail.com>
2021-11-29 19:42:30 -0300

    Create migrations using Entity

---
+++ EntityFramework/AluraStore/Migrations/20211129222527_AddUnityAndRenamePriceOnProducts.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AluraStore.Migrations
+{
+    public partial class AddUnityAndRenamePriceOnProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Products",
+                newName: "UnitPrice");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Unity",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Unity",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "UnitPrice",
+                table: "Products",
+                newName: "Price");
+        }
+    }
+}
